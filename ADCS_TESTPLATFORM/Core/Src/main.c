950e1c5f10252d83e07e8ce9dc5ceef20db93429
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-22 12:17:00 +0200

    HI

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -394,7 +394,7 @@ void SensorReadingTask(void const * argument)
         vTaskDelay(pdMS_TO_TICKS(800));
         meow=HAL_UART_Receive(&huart4,buffer1,1,100);
         len = snprintf(buffer, sizeof(buffer), 
-                      "[%lu] %d\r\n", 
+                      "[%lu] THIS IS THE BYTE:%d\r\n", 
                       (unsigned long)xTaskGetTickCount(),meow);
         HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, HAL_MAX_DELAY);
         uint8_t mtistatus=readIMUPacket(&huart4, gyro, mag, acc, 500); //mag measured in Gauss(G) unit -> 1G = 10^-4 Tesla;
