9ebd09a30aec4c3c0445ce3305134cd5174e1f3c
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-28 15:43:22 +0100

    wadwa

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -82,6 +82,22 @@ static void MX_UART4_Init(void);
 void SensorReadingTask(void const * argument);
 void StartTask02(void const * argument);
 void StartTask03(void const * argument);
+//defining putch to enable printf
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+/*PUTCHAR_PROTOTYPE{
+   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+   return ch;
+}*/
+PUTCHAR_PROTOTYPE{
+	uint8_t c=(uint8_t)ch;
+	sendDriver_UART(&huart2,&c,1);
+	return c;
+}
 
 /* USER CODE BEGIN PFP */
 
