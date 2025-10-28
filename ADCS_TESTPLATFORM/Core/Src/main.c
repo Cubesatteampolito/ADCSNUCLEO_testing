edbf45d71f50fb664ff525661cef3fbf3cd06084
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-28 19:57:54 +0100

    dawadawdwa

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -33,7 +33,9 @@
 #include <malloc.h>
 #include "queue.h"
 #include "queue_structs.h"
-#include "constants.h"
+#include "constants.h"//not really needed but its there anyway
+#include "tim.h"
+#include "usart.h"
 
 
 /* USER CODE END Includes */
@@ -330,7 +332,7 @@ static void MX_USART2_UART_Init(void)
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
   huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
   huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
