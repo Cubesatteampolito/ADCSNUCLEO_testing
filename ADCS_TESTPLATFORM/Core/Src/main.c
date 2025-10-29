098dc1d45b69e263dc130d0d57e068f84189f933
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-29 12:41:48 +0100

    iffy

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -96,12 +96,12 @@ int main(void)
   MX_UART4_Init();
   MX_USART1_UART_Init();
   MX_USART3_UART_Init();
-  MX_CAN1_Init();
+  //MX_CAN1_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
-  MX_ADC1_Init();
-  MX_SPI2_Init();
+ //MX_ADC1_Init();
+  //MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
   
   // Force UART4 to READY state before registering
