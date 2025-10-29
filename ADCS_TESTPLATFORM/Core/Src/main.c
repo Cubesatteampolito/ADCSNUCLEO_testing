068f391662a9a0a2ca6ac86c279d1563b09b381e
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-29 12:52:00 +0100

    Revert "guiiguigu"
    This reverts commit 76cac8e2bb8b9564e1857c71640b119fbca6ecd4.

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
+  * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -19,16 +19,25 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-//#include "adc.h"
-//#include "can.h"
-//#include "spi.h"
-#include "tim.h"
-#include "usart.h"
-#include "gpio.h"
-#include "UARTdriver.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h> //for printf()
+#include <string.h> //for memcpy()
+#include <stdbool.h>
+#include "queue.h"
+#include <math.h>
+#include "UARTdriver.h"
+#include "MTi1.h"
+#include <stm32l452xx.h>
+#include <malloc.h>
+#include "queue.h"
+#include "queue_structs.h"
+#include "constants.h"//not really needed but its there anyway
+#include "tim.h"
+#include "usart.h"
+#include "simpleDataLink.h"
+
 
 /* USER CODE END Includes */
 
@@ -39,6 +48,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -47,20 +57,59 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+//UART_HandleTypeDef huart4;
+//UART_HandleTypeDef huart2;
 
+osThreadId SensorReadingHandle;
+osThreadId task02Handle;
+osThreadId myTask03Handle;
+osMessageQId queue1Handle;
+osMessageQId queue2Handle;
+osMessageQId queue3Handle;
+osMessageQId myQueue04Handle;
+osMessageQId myQueue05Handle;
 /* USER CODE BEGIN PV */
-
+// Defer RX buffering to UARTdriver.c
+// #define UART4_RX_BUF_SIZE 256
+// static uint8_t uart4_rx_ring[UART4_RX_BUF_SIZE];
+// static volatile size_t uart4_rx_head = 0;
+// static volatile size_t uart4_rx_tail = 0;
+// static uint8_t uart4_rx_byte;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 void MX_FREERTOS_Init(void);
+//static void MX_USART2_UART_Init(void);
+//static void MX_UART4_Init(void);
+void SensorReadingTask(void const * argument);
+void StartTask02(void const * argument);
+void StartTask03(void const * argument);
+//defining putch to enable printf
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+/*PUTCHAR_PROTOTYPE{
+   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+   return ch;
+}*/
+PUTCHAR_PROTOTYPE{
+	uint8_t c=(uint8_t)ch;
+	sendDriver_UART(&huart2,&c,1);
+	return c;
+}
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
 
 /* USER CODE END 0 */
 
@@ -70,6 +119,7 @@ void MX_FREERTOS_Init(void);
   */
 int main(void)
 {
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -94,36 +144,84 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_UART4_Init();
-  MX_USART1_UART_Init();
-  MX_USART3_UART_Init();
-  //MX_CAN1_Init();
-  MX_TIM1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
- //MX_ADC1_Init();
-  //MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
-  
-  // Force UART4 to READY state before registering
   huart4.gState = HAL_UART_STATE_READY;
   huart4.RxState = HAL_UART_STATE_READY;
-  
+  // Init and register UART4 with the custom driver
   initDriver_UART();
-  uint8_t status = addDriver_UART(&huart4, UART4_IRQn, keep_new);
-  if (status != 0) {
-    char err[64];
-    int len = snprintf(err, sizeof(err), "addDriver_UART failed: %d\r\n", status);
-    HAL_UART_Transmit(&huart2, (uint8_t*)err, len, 100);
+  if (addDriver_UART(&huart4, UART4_IRQn, keep_new) != 0) {
+    const char* err = "addDriver_UART failed\r\n";
+    HAL_UART_Transmit(&huart2, (uint8_t*)err, strlen(err), 100);
   }
-  /* USER CODE END 2 */
 
+  
   /* Call init function for freertos objects (in freertos.c) */
   MX_FREERTOS_Init();
+
+
+
+  
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* definition and creation of queue1 */
+  osMessageQDef(queue1, 16, uint16_t);
+  queue1Handle = osMessageCreate(osMessageQ(queue1), NULL);
+
+  /* definition and creation of queue2 */
+  osMessageQDef(queue2, 16, uint16_t);
+  queue2Handle = osMessageCreate(osMessageQ(queue2), NULL);
+
+  /* definition and creation of queue3 */
+  osMessageQDef(queue3, 16, uint16_t);
+  queue3Handle = osMessageCreate(osMessageQ(queue3), NULL);
+
+  /* definition and creation of myQueue04 */
+  osMessageQDef(myQueue04, 16, uint16_t);
+  myQueue04Handle = osMessageCreate(osMessageQ(myQueue04), NULL);
+
+  /* definition and creation of myQueue05 */
+  osMessageQDef(myQueue05, 16, uint16_t);
+  myQueue05Handle = osMessageCreate(osMessageQ(myQueue05), NULL);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of SensorReading */
+  osThreadDef(SensorReading, SensorReadingTask, osPriorityNormal, 0, 128);
+  SensorReadingHandle = osThreadCreate(osThread(SensorReading), NULL);
+
+  /* definition and creation of task02 */
+  osThreadDef(task02, StartTask02, osPriorityNormal, 0, 128);
+  task02Handle = osThreadCreate(osThread(task02), NULL);
+
+  /* definition and creation of myTask03 */
+  osThreadDef(myTask03, StartTask03, osPriorityIdle, 0, 128);
+  myTask03Handle = osThreadCreate(osThread(myTask03), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
   osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -180,17 +278,240 @@ void SystemClock_Config(void)
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
-    Error_Handler();//hi
+    Error_Handler();
   }
 }
 
-/* USER CODE BEGIN 4 */
+/**
+  * @brief UART4 Initialization Function
+  * @param None
+  * @retval None
+  */
+// static void MX_UART4_Init(void)
+// {
 
+//   /* USER CODE BEGIN UART4_Init 0 */
+
+//   /* USER CODE END UART4_Init 0 */
+
+//   /* USER CODE BEGIN UART4_Init 1 */
+
+//   /* USER CODE END UART4_Init 1 */
+//   huart4.Instance = UART4;
+//   huart4.Init.BaudRate = 115200;
+//   huart4.Init.WordLength = UART_WORDLENGTH_8B;
+//   huart4.Init.StopBits = UART_STOPBITS_1;
+//   huart4.Init.Parity = UART_PARITY_NONE;
+//   huart4.Init.Mode = UART_MODE_TX_RX;
+//   huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+//   huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+//   huart4.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+//   huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+//   if (HAL_UART_Init(&huart4) != HAL_OK)
+//   {
+//     Error_Handler();
+//   }
+//   /* USER CODE BEGIN UART4_Init 2 */
+
+//   /* USER CODE END UART4_Init 2 */
+
+// }
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+// static void MX_USART2_UART_Init(void)
+// {
+
+//   /* USER CODE BEGIN USART2_Init 0 */
+
+//   /* USER CODE END USART2_Init 0 */
+
+//   /* USER CODE BEGIN USART2_Init 1 */
+
+//   /* USER CODE END USART2_Init 1 */
+//   huart2.Instance = USART2;
+//   huart2.Init.BaudRate = 115200;
+//   huart2.Init.WordLength = UART_WORDLENGTH_8B;
+//   huart2.Init.StopBits = UART_STOPBITS_1;
+//   huart2.Init.Parity = UART_PARITY_NONE;
+//   huart2.Init.Mode = UART_MODE_TX_RX;
+//   huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+//   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+//   huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+//   huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+//   if (HAL_UART_Init(&huart2) != HAL_OK)
+//   {
+//     Error_Handler();
+//   }
+//   /* USER CODE BEGIN USART2_Init 2 */
+
+//   /* USER CODE END USART2_Init 2 */
+
+// }
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD4_Pin */
+  GPIO_InitStruct.Pin = LD4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD4_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF3_I2C4;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+// Remove local HAL UART RX callback to avoid conflicts with UARTdriver.c
+// void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) { /* not used */ }
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_SensorReadingTask */
+/**
+  * @brief  Function implementing the SensorReading thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_SensorReadingTask */
+void SensorReadingTask(void const * argument)
+{
+  /* USER CODE BEGIN 5 */
+  printf("IMU TASK: Starting IMU Configuration... \n");
+  char err[64];
+  //int len = snprintf(err, sizeof(err), "addDriver_UART failed: \r\n");
+  //HAL_UART_Transmit(&huart2, (uint8_t*)err, len, 100);
+  uint8_t ret = initIMUConfig(&huart4);
+  float gyro[3]={1,2,3};
+	float mag[3]={4,5,6};
+	float acc[3] = {7,8,9};
+  char buffer[100];
+
+	imu_queue_struct *local_imu_struct =(imu_queue_struct*) malloc(sizeof(imu_queue_struct));
+
+	/* Infinite loop */
+	for(;;)
+	{
+
+
+		ret=readIMUPacket(&huart4, gyro, mag, acc, 500); //mag measured in Gauss(G) unit -> 1G = 10^-4 Tesla
+		mag[0]/=10000; //1G = 10^-4 Tesla
+		mag[1]/=10000; //1G = 10^-4 Tesla
+		mag[2]/=10000; //1G = 10^-4 Tesla
+
+		
+		if(ret)
+		{
+
+
+			if (local_imu_struct == NULL) {
+				printf("IMU TASK: allocazione struttura fallita !\n");
+			}
+			else
+			{
+				//Riempio struct con valori letti da IMU,per poi inviareli a Task Controllo
+				for (int i = 0; i < 3; i++)
+				{
+					local_imu_struct->gyro_msr[i] = gyro[i];
+					local_imu_struct->mag_msr[i] = mag[i];
+					local_imu_struct->acc_msr[i] = acc[i];
+					//int len = snprintf(buffer, sizeof(err), "addDriver_UART failed: %f\r\n", mag[i]);
+          //printf("Gyroscope axis %d, value %f \r\n", i, acc[i]);
+          //HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, 100);
+					//printf("Gyroscope axis %d, value %f \r\n", i, gyro[i]);
+          //int len = snprintf(buffer, sizeof(err), "Gyroscope axis %d, value %f \r\n", i, gyro[i]);
+          //HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, 100);
+					//printf("Magnetometer axis %d, value %f \r\n", i, mag[i]);
+          int len = snprintf(buffer, sizeof(err), "Magnetometer axis %d, value %f \r\n", i, mag[i]);
+          HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, 100);
+				}
+				
+			}
+		}
+		else{
+			printf("IMU: Error configuring IMU \n");
+			osDelay(2000);
+		}
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartTask02 */
+/**
+* @brief Function implementing the task02 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask02 */
+void StartTask02(void const * argument)
+{
+
+  
+  for(;;)
+  {
+  
+  }
+  /* USER CODE END StartTask02 */
+}
+
+/* USER CODE BEGIN Header_StartTask03 */
+/**
+* @brief Function implementing the task03 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask03 */
+void StartTask03(void const * argument)
+{
+  /* USER CODE BEGIN StartTask03 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartTask03 */
+}
 
 /**
   * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM6 interrupt took place, inside
+  * @note   This function is called  when TIM1 interrupt took place, inside
   * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
   * a global variable "uwTick" used as application time base.
   * @param  htim : TIM handle
@@ -201,7 +522,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM6) {
+  if (htim->Instance == TIM1)
+  {
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
