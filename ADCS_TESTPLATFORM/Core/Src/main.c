f9c5601e3de5f422f2909723f821d9d9f6b037c7
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-29 09:11:55 +0100

    wa

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -79,8 +79,8 @@ osMessageQId myQueue05Handle;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
-static void MX_UART4_Init(void);
+//static void MX_USART2_UART_Init(void);
+//static void MX_UART4_Init(void);
 void SensorReadingTask(void const * argument);
 void StartTask02(void const * argument);
 void StartTask03(void const * argument);
