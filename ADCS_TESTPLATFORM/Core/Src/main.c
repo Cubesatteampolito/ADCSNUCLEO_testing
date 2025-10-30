71327f741102a243b53a7fb34c4f464a0b9758a6
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-30 12:40:49 +0100

    print test 4 try to debug the add driver

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -120,11 +120,17 @@ int main(void)
   MX_UART4_Init();
   /* USER CODE BEGIN 2 */
 
-
-
-
-
-
+  // Force UART4 to READY state before registering
+  huart2.gState = HAL_UART_STATE_READY;
+  huart2.RxState = HAL_UART_STATE_READY;
+  
+  initDriver_UART();
+  uint8_t status = addDriver_UART(&huart2, UART4_IRQn, keep_new);
+  if (status != 0) {
+    char err[64];
+    int len = snprintf(err, sizeof(err), "addDriver_UART failed: %d\r\n", status);
+    HAL_UART_Transmit(&huart2, (uint8_t*)err, len, 100);
+  }
 
   /* USER CODE END 2 */
 
@@ -352,7 +358,7 @@ void StartDefaultTask(void const * argument)
   // making sure that UART driver is initialized and UARTs are added after freertos started
   initDriver_UART();
 	//UART2 = for printf
-	addDriver_UART(&huart2,USART2_IRQn,keep_old);
+	addDriver_UART(&huart2,USART2_IRQn,keep_new);
 	//UART4 = for IMU
 	addDriver_UART(&huart4,UART4_IRQn,keep_new);
   /* Infinite loop */
