c8d4fcc70d4c73ad95d1b65219bd98e6a4def7d9
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-30 11:46:25 +0100

    bracket forgor, print test 4

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -69,7 +69,7 @@ void StartDefaultTask(void const * argument);
 PUTCHAR_PROTOTYPE{
   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
   return ch;
-
+}
 // PUTCHAR_PROTOTYPE{
 // 	uint8_t c=(uint8_t)ch;
 // 	sendDriver_UART(&huart2,&c,1);
