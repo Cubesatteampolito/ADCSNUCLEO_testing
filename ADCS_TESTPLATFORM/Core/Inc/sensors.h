c57165e19ce094deda7987f045a467186b8f63d2
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-10-31 11:54:37 +0100

    imu reading test 2 comment out unused code in queue_structs.h

--- ADCS_TESTPLATFORM/Core/Inc/sensors.h
+++ ADCS_TESTPLATFORM/Core/Inc/sensors.h
@@ -2,7 +2,7 @@
 #define SENSORS_H
 //This is the header file of the library that handles temperature sensors,solar sensors and imu 
 //on the ADCS board
-#include "spi.h"
+//#include "spi.h"
 #include "UARTdriver.h"
 #include <math.h>
 #include <stdio.h> //for printf()
@@ -30,12 +30,12 @@ typedef struct{
 
 }Temp_values;
 //FUNCTIONS
-/**
-  * @brief  Function to initialize all structure and variables needed for management of ADC(internal and external)
-  * @param	Temp values Struct to handle all variables regarding temperature sensors
-  * @retval none
-  */
-void init_tempsens_handler(Temp_values *Temp_values);
+// /**
+//   * @brief  Function to initialize all structure and variables needed for management of ADC(internal and external)
+//   * @param	Temp values Struct to handle all variables regarding temperature sensors
+//   * @retval none
+//   */
+// void init_tempsens_handler(Temp_values *Temp_values);
 
 /**
   * @brief  Function to select which channel of analogic mux send to external ADC
@@ -44,29 +44,29 @@ void init_tempsens_handler(Temp_values *Temp_values);
   */
 void select_input(uint8_t sel);
 
-/**
-  * @brief  Function to make analog-to-digital conversion with external ADC
-  * @param	spi_struct Structure needed to handle SPI communication with external ADC
-  * @param  mode variable to select the working mode of ADC:
-  * 1) mode = 0 : Continuous conversione mode
-  * 2) mode = 1 : Single conversion mode
-  * 3) mode = 2 : Continuous Read mode(Da implementare)
-  * IF mode different from one of these values print an error
-  * @param status_flag a flag to check the status of conversion
-  * @retval none
-  */
-float ADC_Conversion(SPI_HandleTypeDef *spi_struct,uint8_t mode);
-//uint16_t ADC_Conversion(SPI_HandleTypeDef *spi_struct,uint8_t mode);
+// /**
+//   * @brief  Function to make analog-to-digital conversion with external ADC
+//   * @param	spi_struct Structure needed to handle SPI communication with external ADC
+//   * @param  mode variable to select the working mode of ADC:
+//   * 1) mode = 0 : Continuous conversione mode
+//   * 2) mode = 1 : Single conversion mode
+//   * 3) mode = 2 : Continuous Read mode(Da implementare)
+//   * IF mode different from one of these values print an error
+//   * @param status_flag a flag to check the status of conversion
+//   * @retval none
+//   */
+// float ADC_Conversion(SPI_HandleTypeDef *spi_struct,uint8_t mode);
+// //uint16_t ADC_Conversion(SPI_HandleTypeDef *spi_struct,uint8_t mode);
 
 
-/**
-  * @brief  Function to select which channel of analogic mux send to external ADC
-  * @param	sel Variable to select channel(0 to 7)
-  * @retval none
-  */
-void voltage_to_temperature_conv(float value,Temp_values *s1,uint8_t i);
+// /**
+//   * @brief  Function to select which channel of analogic mux send to external ADC
+//   * @param	sel Variable to select channel(0 to 7)
+//   * @retval none
+//   */
+// void voltage_to_temperature_conv(float value,Temp_values *s1,uint8_t i);
 
 
-void get_temperatures(SPI_HandleTypeDef *spi_struct,Temp_values *temp_struct, uint8_t counter);
+// void get_temperatures(SPI_HandleTypeDef *spi_struct,Temp_values *temp_struct, uint8_t counter);
 
 #endif
