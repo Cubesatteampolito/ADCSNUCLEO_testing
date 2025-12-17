2728f6ecf8dc3a77bfb22367a3f8510f7119656a
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2025-12-17 10:24:36 +0100

    maybe

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -949,7 +949,7 @@ void IMU_Task(void const * argument)
 				}
 
         /* BDOT IMPLEMENTATION HERE */
-        compute_mcon(mag, gyro, k, m_con);
+        // compute_mcon(mag, gyro, k, m_con);
         // printf("Dipole Moment: %f %f %f \r\n", m_con[0], m_con[1], m_con[2]);
         // T = m x B
         /* BDOT FINISHED */
@@ -1159,15 +1159,15 @@ void Control_Algorithm_Task(void const * argument)
 		retvalue = osMessageGet(IMUQueue1Handle, 300);
 		processCombinedData((void*)&retvalue,(void *)&local_imu_struct1,receive_IMUqueue_control);
     //algorithm
-    //   for (int i = 0; i < 3; i++)
-    // {
-    //   local_imu_struct1->gyro_msr[i] = gyro[i];
-    //   local_imu_struct1->mag_msr[i] = mag[i];
-    //   local_imu_struct1->acc_msr[i] = acc[i];
-    //   printf("Accelerometer axis %d, value %f \r\n", i, acc[i]);
-    //   printf("Gyroscope axis %d, value %f \r\n", i, gyro[i]);
-    //   printf("Magnetometer axis %d, value %f \r\n", i, mag[i]);
-    // }
+      for (int i = 0; i < 3; i++)
+    {
+      gyro[i] = local_imu_struct1->gyro_msr[i];
+      mag[i] = local_imu_struct1->mag_msr[i];
+      acc[i] = local_imu_struct1->acc_msr[i];
+      // printf("Accelerometer axis %d, value %f \r\n", i, acc[i]);
+      // printf("Gyroscope axis %d, value %f \r\n", i, gyro[i]);
+      // printf("Magnetometer axis %d, value %f \r\n", i, mag[i]);
+    }
 		// //ALGORITHM
     // compute_mcon(mag, gyro, k, m_con);
     // compute_duty_cycle(m_con, coil_turn, coil_area, re_coil, VDD_coil, duty_cycle, direction); 
