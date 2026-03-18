5798611f49382f1d2bc946fabbd5a5c47187f967
Marco Canavero <114088341+marco-canavero@users.noreply.github.com>
2026-03-18 12:19:25 +0100

    comments

--- ADCS_TESTPLATFORM/Core/Src/bdot.c
+++ ADCS_TESTPLATFORM/Core/Src/bdot.c
@@ -68,13 +68,17 @@ void compute_duty_cycle(
 
         /* Compute the current I_cmd needed to achieve the required angular momentum m_con */
         float I_cmd = m_con[i] / (coil_turn[i] * coil_area[i]);         // I = angular_momentum / (turns * area)
+        
+        /* Compute the duty cycle needed to achieve the required current in the coils */
         float duty  = (I_cmd * reg_coil[i]) / VDD_coil[i];              // duty = (I * R) / VDD. Signed result
 
         direction[i] = (duty >= 0.0f) ? 1u : 0u;                        // The sign represents the direction
         duty = fabsf(duty) * 100.0f;                                    // Convert to percentage
-        if (duty > 80.0f) duty = 80.0f;                                 // Clamp to 80% max to avoid over actuation (testing purposes)
+        
+        /* Clamp the duty cycle to 80% to avoid overactuation for testing purposes */
+        if (duty > 80.0f) duty = 80.0f;                                 
 
-        duty_cycle[i] = duty;                                           // [0,80]%
+        duty_cycle[i] = duty;                   // [0,80]%
     }
 }
 
