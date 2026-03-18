73aea1f635c071930701ed9be9983dc7bea3b032
Marco Canavero <114088341+marco-canavero@users.noreply.github.com>
2026-03-18 11:49:37 +0100

    added comments and changed B_dot to b_dot

--- ADCS_TESTPLATFORM/Core/Src/bdot.c
+++ ADCS_TESTPLATFORM/Core/Src/bdot.c
@@ -1,34 +1,50 @@
 /* BDOT Algorithm implementation + duty cycle computation 
 *
-* Data from magnetometer and gyroscope are used to compute angular momentums to apply via the magnetorquers. From the momentum, the duty cycle for the PWM is computed and used to actuate. 
+* Data from magnetometer and gyroscope are used to compute angular momentums to apply via the magnetorquers.  
 *
+* From the momentum, the duty cycle for the PWM is computed and used to actuate. 
+* The magnetorquers should then produce this dipole moment to generate a torque that opposes angular velocity. 
+* 
 */
 
 #include "bdot.h"
 #include "math.h"
 
-
-/* Return the momentum required to compute the duty cycle starting from mag and gyro readings */
-void compute_mcon(
+/* Return the magnetic dipole moment required to compute the duty cycle starting from mag and gyro readings. */
+void compute_mcon
+(
     const float mag[3],         // Magnetometer readings
     const float gyro[3],        // Gyroscope readings
     float k,                    // BDOT gain
     float m_con[3]              // Results buffer
-){
-    float B_norm = sqrt(mag[0] * mag[0] + mag[1] * mag[1] + mag[2] * mag[2]);               // normalizing magnetic field
-    float b[3] = {0,0,0};
-    if(B_norm != 0){                                                                        // avoid division by 0
-        for(int j = 0; j < 3; j++) b[j] = mag[j] / B_norm;                                  // hat{b}
+) {
+    /* compute the norm of the magnetometer vector */
+    float b_norm = sqrt(mag[0] * mag[0] + mag[1] * mag[1] + mag[2] * mag[2]);               // normalizing magnetic field
+    float b[3] = {0, 0, 0};
 
-        m_con[0] = gyro[1] * b[2] - gyro[2] * b[1];
-        m_con[1] = -(gyro[0] * b[2] - gyro[2] * b[0]);                                      // omega x hat{b}
-        m_con[2] = gyro[0] * b[1] - gyro[1] * b[0];
+    if(b_norm != 0)     /* check norm value to avoid division by 0 when normalizing */
+    {                                                                        
+        /* normalize magnetometer readings to get a unit direction vector (b_hat = b / b_norm) */
+        for(int j = 0; j < 3; j++) b[j] = mag[j] / b_norm;                                  
 
-        for(int j = 0; j < 3; j++) {
-            m_con[j] = (m_con[j] * k) / B_norm;                                             // m = (k / norm(B)) * (omega x hat{b}) 
+        /* compute the control dipole moment as the vector product (omega x b_hat) between the angular velocity vector omega and the normalized magnetic field vector b_hat */
+        m_con[0] =   gyro[1] * b[2] - gyro[2] * b[1];
+        m_con[1] = -(gyro[0] * b[2] - gyro[2] * b[0]);                            
+        m_con[2] =   gyro[0] * b[1] - gyro[1] * b[0];
+
+        /* normalize the dipole moment after multiplying it by the BDOT gain */
+        for(int j = 0; j < 3; j++) 
+        {
+            m_con[j] = (m_con[j] * k) / b_norm;                                             // m = (k / norm(B)) * (omega x hat{b}) 
         }
     }
-    else{for(int j = 0; j < 3; j++) m_con[j] = 0;}
+    else
+    {
+        for(int j = 0; j < 3; j++) 
+        {
+            m_con[j] = 0;
+        }
+    }
 }
 
 /* Update duty_cycle[] to configure PWM on the magnetorquers */
@@ -40,7 +56,7 @@ void compute_duty_cycle(
     const float VDD_coil[3],        // Supply voltage for each coil
     float duty_cycle[3],            // Results buffer
     uint8_t direction[3]            // Computed direction for each coil
-){
+) {
     /* For each of the three axes */
     for (int i = 0; i < 3; i++)
     {
