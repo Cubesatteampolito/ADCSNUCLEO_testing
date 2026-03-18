6606c83fb9d6ec5df6dde3488d1640d43db0e1cc
tiz314 <63679072+tiz314@users.noreply.github.com>
2026-03-18 10:11:47 +0100

    still \r

--- ADCS_TESTPLATFORM/Core/Src/queue_structs.c
+++ ADCS_TESTPLATFORM/Core/Src/queue_structs.c
@@ -22,7 +22,7 @@ void receive_IMUqueue_control(
 	{
 		int_queue_struct = (imu_queue_struct *)((osEvent *) event)->value.p;
 		#if ( DEBUG_MSGS == 1 )
-			printf("Control TASK: Received IMU measured values via Queue \n");
+			printf("Control TASK: Received IMU measured values via Queue \r\n");
 		#endif
 		for(int i=0 ; i < 3 ; i++){
 				int_pid_struct->angSpeed_Measured[i] = int_queue_struct->gyro_msr[i];
@@ -56,7 +56,7 @@ void receive_IMUqueue_OBC(
 	{
 		int_queue_struct = (imu_queue_struct *)((osEvent *) event)->value.p;
 		#if ( DEBUG_MSGS == 1 )
-			printf("OBC TASK: Received IMU measured values via Queue \n");
+			printf("OBC TASK: Received IMU measured values via Queue \r\n");
 		#endif
 		int_attitude_struct->omega_x = int_queue_struct->gyro_msr[0];
 		int_attitude_struct->omega_y = int_queue_struct->gyro_msr[1];
@@ -69,12 +69,12 @@ void receive_IMUqueue_OBC(
 		int_attitude_struct->b_y = int_queue_struct->mag_msr[1];
 		int_attitude_struct->b_z = int_queue_struct->mag_msr[2];
 		#if ( DEBUG_MSGS == 1 )
-			printf("OBC: Giro[0] : %f \n",int_attitude_struct->omega_x);
-			printf("OBC: Giro[1] : %f \n",int_attitude_struct->omega_y);
-			printf("OBC: Giro[2] : %f \n",int_attitude_struct->omega_z);
-			printf("OBC: Magn Field[0] : %f \n",int_attitude_struct->b_x);
-			printf("OBC: Magn Field[1] : %f \n",int_attitude_struct->b_y);
-			printf("OBC: Magn Field[2] : %f \n",int_attitude_struct->b_z);
+			printf("OBC: Giro[0] : %f \r\n",int_attitude_struct->omega_x);
+			printf("OBC: Giro[1] : %f \r\n",int_attitude_struct->omega_y);
+			printf("OBC: Giro[2] : %f \r\n",int_attitude_struct->omega_z);
+			printf("OBC: Magn Field[0] : %f \r\n",int_attitude_struct->b_x);
+			printf("OBC: Magn Field[1] : %f \r\n",int_attitude_struct->b_y);
+			printf("OBC: Magn Field[2] : %f \r\n",int_attitude_struct->b_z);
 		#endif
 		free(int_queue_struct);
 		}
@@ -95,21 +95,21 @@ void receive_Current_Tempqueue_OBC(
 	{
 		int_queue_struct = (Current_Temp_Struct *)((osEvent *) event)->value.p;
 		#if ( DEBUG_MSGS == 1 )
-			printf("OBC TASK: Received Currents and Temperatures values via Queue \n");
+			printf("OBC TASK: Received Currents and Temperatures values via Queue \r\n");
 		#endif
 		for(int i=0 ; i < NUM_ACTUATORS + NUM_TEMP_SENS ; i++)
 		{
 			if( i < NUM_ACTUATORS ){
 	    			int_HK_struct->current[i] = int_queue_struct->current[i];
 					#if ( DEBUG_MSGS == 1 )
-						printf("OBC Task: Actuator %d current: %f Current_Temp_buff: %f \n",i+1,int_HK_struct->current[i],int_queue_struct->current[i]);
+						printf("OBC Task: Actuator %d current: %f Current_Temp_buff: %f \r\n",i+1,int_HK_struct->current[i],int_queue_struct->current[i]);
 					#endif
 			}
 	    	else
 	    		if( i < NUM_ACTUATORS + NUM_TEMP_SENS ){
 	    			int_HK_struct->temperature[i - NUM_ACTUATORS] = int_queue_struct->temperature[i - NUM_ACTUATORS];
 					#if ( DEBUG_MSGS == 1 )
-						printf("OBC Task: Temperature n%d value: %f Current_Temp_buff: %f \n",i - 4,int_HK_struct->temperature[i - NUM_ACTUATORS],int_queue_struct->temperature[i - NUM_ACTUATORS]);
+						printf("OBC Task: Temperature n%d value: %f Current_Temp_buff: %f \r\n",i - 4,int_HK_struct->temperature[i - NUM_ACTUATORS],int_queue_struct->temperature[i - NUM_ACTUATORS]);
 					#endif
 	    		}
 		}
