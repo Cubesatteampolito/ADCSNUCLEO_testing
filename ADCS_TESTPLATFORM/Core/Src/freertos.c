907cb4e6bcfe30d4dc159c46c376fad14534c78f
Marco Canavero <114088341+marco-canavero@users.noreply.github.com>
2026-03-24 20:20:42 +0100

    comments

--- ADCS_TESTPLATFORM/Core/Src/freertos.c
+++ ADCS_TESTPLATFORM/Core/Src/freertos.c
@@ -34,7 +34,30 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+/* Defining Task related variables */ 
+osThreadId IMUTaskHandle; 
+uint32_t IMUTaskBuffer[stack_size]; 
+// 4096 osStaticThreadDef_t IMUTaskControlBlock; 
+osThreadId OBC_CommTaskHandle; 
+uint32_t OBC_CommTaskBuffer[stack_size1]; 
+// 16384 osStaticThreadDef_t OBC_CommTaskControlBlock; 
+osThreadId ControlAlgorithmTaskHandle; 
+uint32_t ControlAlgorithmTaskBuffer[stack_size]; 
+// 4096 osStaticThreadDef_t ControlAlgorithmTaskControlBlock; 
+osThreadId FirstCheckTaskHandle; 
+uint32_t FirstCheckTaskBuffer[stack_size]; 
+// 4096 osStaticThreadDef_t FirstCheckTaskControlBlock; 
+xSemaphoreHandle IMURead_ControlMutex; 
+StaticSemaphore_t xIMURead_ControlMutexBuffer; 
+osMessageQId ADCSHouseKeepingQueueHandle; 
+uint8_t ADCSHouseKeepingQueueBuffer[256 * sizeof(float)]; 
+osStaticMessageQDef_t ADCSHouseKeepingQueueControlBlock; 
+osMessageQId IMUQueue2Handle; 
+uint8_t IMUQueue2Buffer[256 * sizeof(imu_queue_struct)];
+osStaticMessageQDef_t IMUQueue2ControlBlock; 
+osMessageQId IMUQueue1Handle; 
+uint8_t IMUQueue1Buffer[256 * sizeof(imu_queue_struct)]; 
+osStaticMessageQDef_t IMUQueue1ControlBlock; 
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
