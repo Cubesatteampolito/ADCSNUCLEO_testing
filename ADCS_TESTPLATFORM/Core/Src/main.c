5b705ecac7922fd429ab405e42dd0de024bfc22e
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2026-04-22 15:19:09 +0200

    maybe the delay itself the problem

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -1190,7 +1190,7 @@ void Control_Algorithm_Task(void const * argument)
     if (test_counter == 0) {
       actuator_START(coils[0]);
       printf("Starting delay\r\n");
-      vTaskDelay(pdMS_TO_TICKS(10000));
+      // vTaskDelay(pdMS_TO_TICKS(10000));
       printf("delay ended\r\n");
       actuator_STOP(coils[0]);
       active[0] = 0;
