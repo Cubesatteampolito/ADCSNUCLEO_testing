d02d62b1566a149d0f14af0dd28d6cee96102769
hikari2407 <93720816+hikari2407@users.noreply.github.com>
2026-04-22 14:27:40 +0200

    status print

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -1180,6 +1180,7 @@ void Control_Algorithm_Task(void const * argument)
   //Inizialize PID struct
   PID_INIT(&PID_Inputs);
   int test_counter=0;
+  printf("i am alive\r\r");
   if (test_counter == 0) {
     actuator_START(coils[0]);
     active[0] = 1;
