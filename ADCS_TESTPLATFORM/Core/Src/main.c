3c9b49658f477d29294dadbd5d10f4a68e75ce69
shihabalratul <shihabalratul@gmail.com>
2026-04-22 17:29:26 +0200

    Trying without any delay

--- ADCS_TESTPLATFORM/Core/Src/main.c
+++ ADCS_TESTPLATFORM/Core/Src/main.c
@@ -1195,7 +1195,7 @@ void Control_Algorithm_Task(void const * argument)
     if (test_counter == 0) {
       // actuator_START(coils[0]);
       printf("Starting delay\r\n");
-      osDelay(100);
+      // osDelay(100);
       // 
       // vTaskDelay(pdMS_TO_TICKS(10000));
       printf("delay ended\r\n");
